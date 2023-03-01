8526a82fe0406ab67f1032d459a72341df3f767b
Milton Figueredo Miles <gratno@gmail.com>
2023-03-01 17:52:30 -0500

    Solucionando error archivo Application Properties

--- src/main/java/com/alura/forum/model/Curso.java
+++ src/main/java/com/alura/forum/model/Curso.java
@@ -1,9 +1,11 @@
 package com.alura.forum.model;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 
 @Entity
 public class Curso {
