002d57a0e5676fc2e21127b7ba8696434c129bba
Milton Figueredo Miles <gratno@gmail.com>
2023-03-24 18:26:14 -0500

    MethodArgumentNotValidException

--- src/main/java/com/alura/forum/config/validation/ApplicationErrorHandler.java
+++ src/main/java/com/alura/forum/config/validation/ApplicationErrorHandler.java
@@ -1,11 +1,23 @@
 package com.alura.forum.config.validation;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.alura.forum.controller.validation.FormError;
 
 @RestControllerAdvice
 public class ApplicationErrorHandler {
 
-	public void handlerValidation() {
+	
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<FormError> handlerValidation(MethodArgumentNotValidException exception) {
+		
 		
 		
 	}
---
+++ src/main/java/com/alura/forum/controller/validation/FormError.java
@@ -0,0 +1,9 @@
+package com.alura.forum.controller.validation;
+
+public class FormError {
+
+	private String campo;
+	
+	private String mensaje;
+	
+}
